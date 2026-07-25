7dc359e766bf569d477e304c1d002f070ff54892
anishakapoor25-gif <anishakapoor25@navgurukul.org>
2026-07-25 12:14:38 +0530

    Updated API URL to Railway

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://e-commerce-api-production-41f1.up.railway.app",
 });
 
 export default api;
